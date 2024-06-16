3b5e7e175baed42885abd5985ebaf62be9c9096f
dfrojas20 <123292688+dfrojas20@users.noreply.github.com>
2024-06-16 15:52:29 -0400

    test7

--- server/test.js
+++ server/test.js
@@ -1,5 +1,13 @@
 // curl -s -H "Accept: application/json" -X POST -d '{"numbers": [1,1,2,3,5]}' POST http://localhost:3000/dev/calcs/add --connect-timeout 30
 
+const axios = require('axios').default
+  res = await axios.post('http://localhost:3000/dev/calcs/add', {
+    numbers: [1,1,2,3,5],
+})
+
+console.log(res);
+
+
 describe('basics', () => {
 
 it('test add', async () => {
